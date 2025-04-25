7908fe457e7ed33068584c0949ebbc1b7786fbaa
develckm86 <develckm86@gmail.com>
2025-04-25 16:24:40 +0900

    회원가입 주소검색 추가

--- src/pages/app/base/Signup.jsx
+++ src/pages/app/base/Signup.jsx
@@ -4,13 +4,38 @@ import React from "react"
 
 import { useState } from "react"
 import {Link, useNavigate} from "react-router"
-
+  import DaumPostcode from "react-daum-postcode"
 export default function SignupPage() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
   const [error, setError] = useState("")
   const navigate = useNavigate()
+  const [address, setAddress] = useState("")
+  const [zonecode, setZonecode] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const themeObj = {
+    bgColor: '#FFFFFF',
+    pageBgColor: '#FFFFFF',
+    postcodeTextColor: '#C05850',
+    emphTextColor: '#222222',
+  };
+
+  const postCodeStyle = {
+    width: '360px',
+    height: '480px',
+  };
+
+  const completeHandler = (data) => {
+    const { address, zonecode } = data;
+    setZonecode(zonecode);
+    setAddress(address);
+  };
+
+  const toggleHandler = () => {
+    setIsOpen((prevOpenState) => !prevOpenState);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -54,6 +79,31 @@ export default function SignupPage() {
               </Link>
             </p>
           </div>
+          {isOpen && (
+              <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/80 bg-opacity-50">
+                <div className="bg-white p-6 rounded-lg shadow-lg w-[400px] max-h-[90vh] overflow-auto relative">
+                  <button
+                      className="absolute top-2 right-2 text-gray-500 hover:text-black"
+                      onClick={toggleHandler}
+                  >
+                    ✕
+                  </button>
+
+                  <DaumPostcode
+                      theme={themeObj}
+                      style={postCodeStyle}
+                      onComplete={completeHandler}
+                      onClose={(state) => {
+                        if (state === 'FORCE_CLOSE') {
+                          setIsOpen(false);
+                        } else if (state === 'COMPLETE_CLOSE') {
+                          setIsOpen(false);
+                        }
+                      }}
+                  />
+                </div>
+              </div>
+          )}
           <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
@@ -70,6 +120,52 @@ export default function SignupPage() {
                     placeholder="이메일"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="flex">
+                <label htmlFor="address" className="sr-only">
+                  주소
+                </label>
+                <input
+                    id="address"
+                    name="address"
+                    type="text"
+                    required
+                    className="appearance-none rounded-none relative  px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
+                    placeholder="주소"
+                    value={address}
+                    readOnly={true}
+                />
+                <input
+                    id="zonecode"
+                    name="zonecode"
+                    type="text"
+                    required
+                    className="appearance-none rounded-none relative  px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
+                    placeholder="지번"
+                    value={zonecode}
+                    readOnly={true}
+
+                />
+                <button
+                    onClick={toggleHandler}
+                    type="button"
+                    className="group relative  flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
+                >
+                  주소 찾기
+                </button>
+              </div>
+              <div>
+                <label htmlFor="detail-address" className="sr-only">
+                  비밀번호
+                </label>
+                <input
+                    id="detail-address"
+                    name="detailAddress"
+                    type="text"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
+                    placeholder="상세주소"
                 />
               </div>
               <div>
